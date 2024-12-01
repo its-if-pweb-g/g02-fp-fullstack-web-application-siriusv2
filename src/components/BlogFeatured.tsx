9458fe6af9f424ebdb2a1f3d4a5e98aa8285ba7c
kevin2807-cmd <kl905577@gmail.com>
2024-12-01 17:43:07 +0700

    fix: Responsive design blog

--- src/components/BlogFeatured.tsx
+++ src/components/BlogFeatured.tsx
@@ -2,37 +2,36 @@ import React from "react";
 import Image from "next/image";
 
 export default function BlogFeatured() {
-    return (
-        <div className="mx-44 items-center mb-10">
-            <h1 className="py-10 text-5xl font-bold">
-                This is the Sirius project V2. Stay tunned!!.
-            </h1>
-            <div className="flex gap-5 items-center justify-center ">
-                <div className="w-1/2">
-                    <Image
-                        src={"/blog/contoh.jpeg"}
-                        alt="contoh"
-                        width={450}
-                        height={500}
-                        className="rounded-lg"
-                    />
-                </div>
-                <div className="w-1/2">
-                    <h1 className="font-bold text-2xl mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex
-                        ea commodo consequat.
-                    </p>
-                    <button className="transition duration-300 ease-in-out p-3 rounded-lg my-3 shadow-lg">
-                        Read more
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="mx-16 lg:mx-44 items-center mb-10">
+      <h1 className="md:text-5xl py-10 text-2xl font-bold">
+        This is the Sirius project V2. Stay tunned!!.
+      </h1>
+      <div className="md:flex md:gap-5 md:items-center md:justify-center ">
+        <div className="w-1/2">
+          <Image
+            src={"/blog/contoh.jpeg"}
+            alt="contoh"
+            width={450}
+            height={500}
+            className="rounded-lg hidden md:block"
+          />
         </div>
-    );
+        <div className="md:w-1/2 w-full">
+          <h1 className="font-bold lg:text-2xl mb-4 text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button className="transition duration-300 ease-in-out p-3 rounded-lg my-3 shadow-lg">
+            Read more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,27 +4,45 @@ import Image from "next/image";
 
 export default function Category() {
   return (
-    <div className="mx-44 mb-10">
+    <div className="md:mx-44 mb-10 mx-16">
       <h1 className="text-xl font-semibold mb-7">Top Visited</h1>
       <div className="flex flex-wrap gap-4">
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           Sport
-        </div>
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        </Link>
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           Programming
-        </div>
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        </Link>
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           CyberSecurity
-        </div>
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        </Link>
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           AI
-        </div>
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        </Link>
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           Healthy food
-        </div>
-        <div className="w-[23%] h-12 rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md">
+        </Link>
+        <Link
+          href="#"
+          className="md:w-[23%] h-12 w-full rounded-lg items-center text-center flex justify-center  bg-indigo-100 text-gray-700 font-semibold hover:bg-indigo-50 transition duration-300 ease-in-out shadow-md"
+        >
           Junk food
-        </div>
+        </Link>
       </div>
     </div>
   );
