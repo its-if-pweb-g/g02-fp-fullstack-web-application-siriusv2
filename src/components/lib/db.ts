2bb91c5461028b34100df54ffcd1377f0acace2b
CarolusNathanell <c.nathanell05@gmail.com>
2024-12-02 11:27:55 +0700

    fix: fixed mongodb?

--- src/components/lib/db.ts
+++ src/components/lib/db.ts
@@ -3,7 +3,7 @@ import { MongoClient, Db } from "mongodb";
 let db: Db | null = null;
 
 export const connectDb = (cb: (err?: Error | null) => void): void => {
-  MongoClient.connect("mongodb+srv://Sirius:Siriusv2@sirius.eypym.mongodb.net/?retryWrites=true&w=majority&appName=Sirius")
+  MongoClient.connect("mongodb://Sirius:Siriusv2@sirius-shard-00-00.eypym.mongodb.net:27017,sirius-shard-00-01.eypym.mongodb.net:27017,sirius-shard-00-02.eypym.mongodb.net:27017/?ssl=true&replicaSet=atlas-ti5k9g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Sirius")
     .then((client) => {
       db = client.db();
       cb(null);
