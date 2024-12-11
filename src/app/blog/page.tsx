338ed88f06525cc3033b3a0cf23ea06a191395ab
kevin2807-cmd <kl905577@gmail.com>
2024-12-11 15:05:20 +0700

    Fix blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,9 @@
 import BlogFeatured from "@/components/blog/BlogFeatured";
-import Category from "@/components/blog/Category";
 
 export default function Page() {
   return (
     <div className="landing bg-gradient-to-r from-[#3872be] min-h-screen to-[#bde7ff]">
       <BlogFeatured />
-      <Category />
     </div>
   );
 }
